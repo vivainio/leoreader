4fe74c96572b903899584191078ea4f75a5ef7f3
Ville M. Vainio <vivainio@gmail.com>
2013-04-20 16:49:37 +0300

    add lunchcontent

---
+++ src/com/w/LunchContent.java
@@ -0,0 +1,48 @@
+package com.w;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.tantalum.util.L;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class LunchContent {
+
+	
+	public static Map<String, List<Course>> ITEMS = new HashMap<String, List<Course>>(); 
+	/**
+	 * An array of sample (dummy) items.
+	 */
+	//public static List<Course> ITEMS = new ArrayList<Course>();
+
+	/**
+	 * A map of sample (dummy) items, by ID.
+	 */
+	
+	public static Map<String, Course> ITEM_MAP = new HashMap<String, Course>();
+
+	static {
+		
+		// Add 3 sample items.
+		//addItem(new Course("1", "Hermia 3"));
+		//addItem(new Course("2", "Hermia 6"));
+		//addItem(new DummyItem("3", "Item 3"));
+	}
+
+	private static void addItem(Course item) {
+		//ITEMS.add(item);
+		//ITEM_MAP.put(item.id, item);
+	}
+	
+	public static void dump() {
+		L.i("dump LuncContent", ITEMS.toString());
+	}
+
+}
